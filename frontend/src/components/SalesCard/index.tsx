c7b4af39a40c085fa8289ec61eed258a3c3c1090
Caio Costa <caiomarcelomadeira@hotmail.com>
2022-07-17 00:55:37 -0300

    Axios, useEffect first request

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,19 +1,23 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from '../NotificationButton';
 import './styles.css';
 
-
 function SalesCard() {
 
     const max = new Date();
     const min = new Date(new Date().setDate(new Date().getDate() - 365));;
-
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
-
+    useEffect(() => {
+        axios.get("https://dsmeta-caiommc.herokuapp.com/sales")
+            .then(response => {
+                console.log(response.data);
+            })
+    }, []);
 
     return (
         <div className="dsmeta-card">
@@ -22,7 +26,7 @@ function SalesCard() {
                 <div className="dsmeta-form-control-container">
                     <DatePicker
                         selected={minDate}
-                        onChange={(date: Date) => setMinDate(date) }
+                        onChange={(date: Date) => setMinDate(date)}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
